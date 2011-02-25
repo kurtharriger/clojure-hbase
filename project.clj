c64c70d5ccbb18397db3c900d0b5f73552151948
Kurt Harriger <kurtharriger@gmail.com>
2011-02-24 22:12:02 -0700

    update zookeeper

--- project.clj
+++ project.clj
@@ -5,7 +5,7 @@
   :dependencies [[org.clojure/clojure "1.2.0"]
 		 [org.clojure/clojure-contrib "1.2.0"]
 		 [org.apache.mahout.hadoop/hadoop-core "0.20.1"]
-                 [org.apache.zookeeper/zookeeper "3.3.1"]
+                 [org.apache.zookeeper/zookeeper "3.3.2"]
                  [org.apache.hbase/hbase "0.90.1"]
                  [log4j/log4j "1.2.15"
                   :exclusions [javax.mail/mail
