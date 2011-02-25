516f71b207b9ea1f5d3526a6850f3276257632d6
Kurt Harriger <kurtharriger@gmail.com>
2011-02-24 21:14:55 -0700

    add hadoop-core dependency

--- project.clj
+++ project.clj
@@ -1,7 +1,10 @@
+
+
 (defproject clojure-hbase "0.20.2"
   :description "A convenient Clojure interface to HBase."
   :dependencies [[org.clojure/clojure "1.2.0"]
 		 [org.clojure/clojure-contrib "1.2.0"]
+		 [org.apache.mahout.hadoop/hadoop-core "0.20.1"]
 		 [commons-logging/commons-logging "1.0.4"]])
 
 (ns leiningen.docs)
