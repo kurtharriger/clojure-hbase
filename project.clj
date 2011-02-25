6090520f4129be1b360cef9eff49bef502e2c5e3
Kurt Harriger <kurtharriger@gmail.com>
2011-02-24 21:49:49 -0700

    add hbase dep

--- project.clj
+++ project.clj
@@ -6,6 +6,7 @@
 		 [org.clojure/clojure-contrib "1.2.0"]
 		 [org.apache.mahout.hadoop/hadoop-core "0.20.1"]
                  [org.apache.zookeeper/zookeeper "3.3.1"]
+                 [org.apache/hbase "0.89-SNAPSHOT"]
                  [log4j/log4j "1.2.15"
                   :exclusions [javax.mail/mail
                              javax.jms/jms
