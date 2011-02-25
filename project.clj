16840d346d0d284d0e2975457e74c60b301fbfd0
Kurt Harriger <kurtharriger@gmail.com>
2011-02-24 21:27:36 -0700

    add log4j with exclusions

--- project.clj
+++ project.clj
@@ -5,7 +5,12 @@
   :dependencies [[org.clojure/clojure "1.2.0"]
 		 [org.clojure/clojure-contrib "1.2.0"]
 		 [org.apache.mahout.hadoop/hadoop-core "0.20.1"]
-		 [commons-logging/commons-logging "1.0.4"]])
+                 [log4j/log4j "1.2.15"
+                  :exclusions [javax.mail/mail
+                             javax.jms/jms
+                             com.sun.jdmk/jmxtools
+                             com.sun.jmx/jmxri]]
+		 [commons-logging/commons-logging "1.1"]])
 
 (ns leiningen.docs)
 ;(defn docs...)
